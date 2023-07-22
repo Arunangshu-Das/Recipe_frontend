e11665670a3489f712b6ae423bb7a552d2bdc2f8
Anik Bhattacharya <anikbhattachar27@gmail.com>
2023-07-22 10:34:47 +0530

    first commit

---
+++ src/pages/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+const Search = () => {
+  return (
+    <Layout>
+      <h1>Search Recipe</h1>
+    </Layout>
+  )
+}
+
+export default Search
