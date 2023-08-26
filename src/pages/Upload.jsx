3df726630bae8879cf0319bd48a1498c8c09f54c
Anik Bhattacharya <anikbhattachar27@gmail.com>
2023-08-26 13:06:38 +0530

    first commit

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -5,10 +5,12 @@ const Upload = () => {
     <div className='container'>
       <form className='uploadform'>
         <h1>Create Your Recipe</h1>
+        <div className='form-box'>
         <input type='text' placeholder='name of the recipe' value=''/>
         <input type='text' placeholder='taste' value=''/>
         <textarea rows='6' cols='50' placeholder='About the recipe' value=''/>
         <input type='button' placeholder='Submit' value=''/>
+        </div>
       </form>
     </div>
   )
